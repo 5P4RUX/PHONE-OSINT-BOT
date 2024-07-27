dc9bfe8ab131efed0666720f0a751af93869485e
SPARUX666 <147306534+Sparux-666@users.noreply.github.com>
2024-07-27 12:47:59 +0300

    PHONE-TRACK.py

--- PHONE-TRACK.py
+++ PHONE-TRACK.py
@@ -1,6 +1,6 @@
 import logging
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, CallbackContext
+from telegram.ext import Application, CommandHandler, ContextTypes
 import phonenumbers
 from phonenumbers import carrier, geocoder, timezone
 from pyfiglet import Figlet
@@ -8,31 +8,36 @@ import shutil
 import json
 import os
 
+# Logging ayarları
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+# Define color codes
 LightGreen = "\033[92m"
 DarkGray = "\033[90m"
 White = "\033[97m"
 Red = "\033[91m"
 
 def print_banner():
+    # Get terminal width
     terminal_width = shutil.get_terminal_size().columns
     fig = Figlet(font="smslant")
     banner = fig.renderText('PHONE-TRACK')
+    # Center the banner according to terminal width
     banner_lines = banner.split('\n')
     centered_banner = '\n'.join(line.center(terminal_width) for line in banner_lines)
     print(f"{LightGreen}{centered_banner}{White}")
     
+    # Print GitHub link and tool description centered
     github_line = "GitHub: SPARUX-666".center(terminal_width)
     tool_description_line = "PHONE NUMBER OSINT TOOL".center(terminal_width)
     print(f"{Red}{github_line}{White}")
     print(f"{Red}{tool_description_line}{White}")
 
-def start(update: Update, context: CallbackContext):
-    update.message.reply_text(
-        "Welcome to the Phone-Track Bot! 📱\n\n"
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
+         "Welcome to the Phone-Track Bot! 📱\n\n"
         "This bot provides detailed information about phone numbers using OSINT (Open Source Intelligence) techniques. "
         "You can use this bot to find the carrier, location, timezone, and validity of any phone number around the world.\n\n"
         "Available commands:\n"
@@ -40,8 +45,8 @@ def start(update: Update, context: CallbackContext):
         "/help - DISPLAYS THE HELP MENU\n"
     )
 
-def help_command(update: Update, context: CallbackContext):
-    update.message.reply_text(
+async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await update.message.reply_text(
         "Phone-Track Bot Help\n\n"
         "This bot provides detailed information about phone numbers. Here are the available commands:\n\n"
         "/start - Display the welcome message\n\n"
@@ -51,16 +56,18 @@ def help_command(update: Update, context: CallbackContext):
         "The /phone command provides information such as location, region code, time zone, operator, and validity of the phone number."
     )
 
-def phone_info(update: Update, context: CallbackContext):
+async def phone_info(update: Update, context: ContextTypes.DEFAULT_TYPE):
     if context.args:
         user_phone = ' '.join(context.args)
         
         try:
+            # Parse the entered phone number
             parsed_number = phonenumbers.parse(user_phone)
         except phonenumbers.phonenumberutil.NumberParseException:
-            update.message.reply_text("Invalid phone number format. Please enter the correct format.")
+            await update.message.reply_text("Invalid phone number format. Please enter the correct format.")
             return
 
+        # Retrieve information about the phone number
         region_code = phonenumbers.region_code_for_number(parsed_number)
         operator = carrier.name_for_number(parsed_number, "en")
         location = geocoder.description_for_number(parsed_number, "en")
@@ -72,6 +79,7 @@ def phone_info(update: Update, context: CallbackContext):
         timezones = timezone.time_zones_for_number(parsed_number)
         timezone_str = ', '.join(timezones)
 
+        # Create information dictionary
         phone_info_dict = {
             "Location": location,
             "Region Code": region_code,
@@ -91,33 +99,38 @@ def phone_info(update: Update, context: CallbackContext):
             }.get(number_type, "Other number type")
         }
 
+        # Save to JSON file
         file_path = 'phone_info.json'
         with open(file_path, 'w') as json_file:
             json.dump(phone_info_dict, json_file, indent=4)
 
+        # Send the JSON file to the user
         with open(file_path, 'rb') as json_file:
-            update.message.reply_document(document=json_file, filename='sparux-666-info.json')
+            await update.message.reply_document(document=json_file, filename='sparux-666-info.json')
 
+        # Optionally, you can delete the file after sending
         os.remove(file_path)
     else:
-        update.message.reply_text("Please provide a phone number. Usage: /phone <phone_number>")
+        await update.message.reply_text("Please provide a phone number. Usage: /phone <phone_number>")
 
-def main():
+async def main():
+    # Print the banner
     print_banner()
     
+    # Bot token'ı kullanıcıdan alınacak
     token = input("ENTER BOT TOKEN: ")
 
-    updater = Updater(token, use_context=True)
+    # Create Application and pass it your bot's token.
+    application = Application.builder().token(token).build()
 
-    dp = updater.dispatcher
-
-    dp.add_handler(CommandHandler("start", start))
-    dp.add_handler(CommandHandler("help", help_command))
-    dp.add_handler(CommandHandler("phone", phone_info))
+    # Register handlers
+    application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("help", help_command))
+    application.add_handler(CommandHandler("phone", phone_info))
     
-    updater.start_polling()
-
-    updater.idle()
+    # Start the Bot
+    await application.run_polling()
 
 if __name__ == '__main__':
-    main()
+    import asyncio
+    asyncio.run(main())
